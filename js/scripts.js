368ee57a5f52297d10b1c0225c474aff95587e89
TK <tk@tk.pl>
2017-07-08 16:43:44 +0200

    zatwierdzone

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+﻿var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = femaleNames.concat(maleNames);
+var newName = 'Marian'
+
+if (allNames.indexOf(newName) === -1) {
+            allNames.push(newName)
+            console.log(allNames);
+}
+else {
+            console.log('Imie istnieje');
+}
